3ac49f39933bcb2e1f98985de655f6f07cdd71de
Arsenii Tolmachev <arseniit@gmail.com>
2018-06-20 21:02:38 -0400

    config

---
+++ includes/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+  'title' => 'IT Blog',
+  'db' => array(
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'name' => 'it_blog'  
+  )
+);
